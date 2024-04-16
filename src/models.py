e6e125a8a9930a59f68bc886ed5e2575e7a91de6
Alvaro Ponce Marin <147168257+superteclas@users.noreply.github.com>
2024-04-16 21:30:43 +0000

    tablas relacionadas

--- src/models.py
+++ src/models.py
@@ -44,6 +44,61 @@ class Characters_Favoritos(Base):
     person_id = Column(Integer, ForeignKey('characters.id'))
     person = relationship(User)
 
+class Characters(Base):
+    __tablename__ = 'planets'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    diameter = Column(String(250))
+    rotation_period = Column(String(250))
+    orbital_operiod = Column(String(250))
+    gravity = Column(String(250))
+    population = Column(String(250))
+    climate = Column(String(250))
+    terrain = Column(String(250))
+    surface_water = Column(String(250))
+    person_id = Column(Integer, ForeignKey('user.id'))
+    person = relationship(User)
+class PlanetFavorites(Base):
+    __tablename__ = 'planet_favorites'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    planet_id = Column(Integer, ForeignKey('planets.id'))
+    person_id = Column(Integer, ForeignKey('user.id'))
+    person = relationship(User)
+
+class Vehicles (Base):
+    __tablename__ = 'vehicles'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    model = Column(String(250))
+    vehicle_class = Column(String(250))
+    manufacturer = Column(String(250))
+    cost_in_credits = Column(String(250))
+    lenght = Column(String(250))
+    crew = Column(String(250))
+    passengers = Column(String(250))
+    max_speed = Column(String(250))
+    cargo_capacity = Column(String(250))
+    consumables = Column(String(250))
+    person_id = Column(Integer, ForeignKey('user.id'))
+    person = relationship(User)
+
+class VehicleFavorites(Base):
+    __tablename__ = 'vehicle_favorites'
+        # Here we define columns for the table address.
+        # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    vehicle_id = Column(Integer, ForeignKey('vehicles.id'))
+    person_id = Column(Integer, ForeignKey('user.id'))
+    person = relationship(User)
+
 
     def to_dict(self):
         return {}
