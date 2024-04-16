b005c5af0359c6d86f3310b90e0c980a9ac03059
Alvaro Ponce Marin <147168257+superteclas@users.noreply.github.com>
2024-04-16 21:14:45 +0000

    tablas characters relacionadas

--- src/models.py
+++ src/models.py
@@ -12,18 +12,38 @@ class User(Base):
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    user_name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    favoritesPerson = relationship('Characters_favoritos', backref='user', lazy=True)
+    favorites = relationship('favorites', backref='user', lazy=True)
 
-class Address(Base):
+class Characters(Base):
     __tablename__ = 'characters'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
+    name = Column(String(250))
+    height = Column(String(250))
+    mass = Column(String(250))
+    hair_color = Column(String(250))
+    skin_color = Column(String(250))
+    eye_color = Column(String(250))
+    birth_year = Column(String(250))
+    gender = Column(String(250))
     person_id = Column(Integer, ForeignKey('user.id'))
-    person = relationship(Person)
+    person = relationship(User)
+
+class Characters_Favoritos(Base):
+    __tablename__ = 'Characters_favoritos'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    Characters_Favoritos_id = Column(Integer, ForeignKey('character.id'))
+    person_id = Column(Integer, ForeignKey('characters.id'))
+    person = relationship(User)
+
 
     def to_dict(self):
         return {}
